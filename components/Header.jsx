0c4792368627dc2b00e291a1e194e9b382cf8df2
alelimiranda <alelimiranda227@gmail.com>
2024-07-28 18:05:53 +0800

    feat: Add Links, Pages and Transitions

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+//components
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/*Logo*/}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Aleli<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+
+        {/*Desktop Nav & Hire Me Button*/}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire Me</Button>
+          </Link>
+        </div>
+
+        {/*Mobile Nav*/}
+        <div className="xl:hidden">Mobile Nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
